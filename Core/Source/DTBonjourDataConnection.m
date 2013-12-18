59bc7922ccb92e41eaa7fe41ebf882dc340d44c4
Maximilian Szengel <szengel@equinux.com>
2013-12-18 16:27:18 +0100

    Re-add NSNetService patch with a runtime check for >=iOS7/OSX10.9.

--- Core/Source/DTBonjourDataConnection.m
+++ Core/Source/DTBonjourDataConnection.m
@@ -12,8 +12,87 @@
 
 #import <Foundation/NSJSONSerialization.h>
 
+#define kDTBonjourQNetworkAdditionsCheckSEL NSSelectorFromString(@"netService:didAcceptConnectionWithInputStream:outputStream:")
+
 CGFloat DTBonjourDataConnectionDefaultTimeout = 60.0;
 NSString * DTBonjourDataConnectionErrorDomain = @"DTBonjourDataConnection";
+
+@interface NSNetService (QNetworkAdditions)
+ 
+- (BOOL)qNetworkAdditions_getInputStream:(out NSInputStream **)inputStreamPtr 
+    outputStream:(out NSOutputStream **)outputStreamPtr;
+ 
+@end
+ 
+@implementation NSNetService (QNetworkAdditions)
+ 
+- (BOOL)qNetworkAdditions_getInputStream:(out NSInputStream **)inputStreamPtr 
+    outputStream:(out NSOutputStream **)outputStreamPtr
+    // The following works around three problems with 
+    // -[NSNetService getInputStream:outputStream:]:
+    //
+    // o <rdar://problem/6868813> -- Currently the returns the streams with 
+    //   +1 retain count, which is counter to Cocoa conventions and results in 
+    //   leaks when you use it in ARC code.
+    //
+    // o <rdar://problem/9821932> -- If you create two pairs of streams from 
+    //   one NSNetService and then attempt to open all the streams simultaneously, 
+    //   some of the streams might fail to open.
+    //
+    // o <rdar://problem/9856751> -- If you create streams using 
+    //   -[NSNetService getInputStream:outputStream:], start to open them, and 
+    //   then release the last reference to the original NSNetService, the 
+    //   streams never finish opening.  This problem is exacerbated under ARC 
+    //   because ARC is better about keeping things out of the autorelease pool.
+{
+    BOOL                result;
+    CFReadStreamRef     readStream;
+    CFWriteStreamRef    writeStream;
+ 
+    result = NO;
+    
+    readStream = NULL;
+    writeStream = NULL;
+    
+    if ( (inputStreamPtr != NULL) || (outputStreamPtr != NULL) ) {
+        CFNetServiceRef     netService;
+ 
+        netService = CFNetServiceCreate(
+            NULL, 
+            (__bridge CFStringRef) [self domain], 
+            (__bridge CFStringRef) [self type], 
+            (__bridge CFStringRef) [self name], 
+            0
+        );
+        if (netService != NULL) {
+            CFStreamCreatePairWithSocketToNetService(
+                NULL, 
+                netService, 
+                ((inputStreamPtr  != nil) ? &readStream  : NULL), 
+                ((outputStreamPtr != nil) ? &writeStream : NULL)
+            );
+            CFRelease(netService);
+        }
+        
+        // We have failed if the client requested an input stream and didn't 
+        // get one, or requested an output stream and didn't get one.  We also 
+        // fail if the client requested neither the input nor the output 
+        // stream, but we don't get here in that case.
+        
+        result = ! ((( inputStreamPtr != NULL) && ( readStream == NULL)) || 
+                    ((outputStreamPtr != NULL) && (writeStream == NULL)));
+    }
+    if (inputStreamPtr != NULL) {
+        *inputStreamPtr  = CFBridgingRelease(readStream);
+    }
+    if (outputStreamPtr != NULL) {
+        *outputStreamPtr = CFBridgingRelease(writeStream);
+    }
+    
+    return result;
+}
+ 
+@end
 
 @interface DTBonjourDataConnection () <NSStreamDelegate>
 
@@ -85,10 +164,23 @@ typedef enum
 	{
   	NSInputStream *in;
     NSOutputStream *out;
-		if (![service getInputStream:&in outputStream:&out])
-		{
-			return nil;
-		}
+
+    if (![[service delegate] respondsToSelector:kDTBonjourQNetworkAdditionsCheckSEL])
+    {
+      // Older iOS/OSX versions need a patch for getting input and output
+      // streams see the `QNetworkAdditions` above. (If the delegate does not
+      // implement the `kDTBonjourQNetworkAdditionsCheck` selector, we can
+      // simply use the patched version.
+      if ([service qNetworkAdditions_getInputStream:&in outputStream:&out])
+        return nil;
+    }
+    else
+    {
+      // iOS7/OSX10.9
+      if (![service getInputStream:&in outputStream:&out])
+        return nil;
+    }
+
 		_inputStream = in;
     _outputStream = out;
 		_outputQueue = [[NSMutableArray alloc] init];
